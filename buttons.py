e7e5d18e427b45451566bd4652706da8348d25dc
3n9ff <979pol@gmail.com>
2021-08-06 19:13:16 +0200

    oleeeee, el texto funciona, pero es horrible

--- buttons.py
+++ buttons.py
@@ -16,18 +16,18 @@ class Startbutton:
         self.font = pygame.font.SysFont(None, 50)
         self.txrender = self.font.render( self.msg, True, self.txcolor, (0,0,0))
         self.txrect = self.txrender.get_rect()
-        self.txrect.centerx = self.screen_rect.center.x
-        self.txrect.centery = self.screen_rect.center.x
+        self.txrect.centerx = self.screen_rect.centerx
+        self.txrect.centery = self.screen_rect.centery
 
         #Rect info
-        self.reccolor = (0,0,0)        
+        self.reccolor = (0,0,0) 
+        self.rectangulo = pygame.draw.rect(self.screen, self.reccolor, self.txrect, border_radius=10)       
     #TRABAJA X AKI, NO FUNCIONA
 
     def draw_button(self):
         """Draw the start button"""
         #Display the text
-        self.screen.blit(self.txrect, self.txrender)
+        self.screen.blit(self.txrender, self.txrect)
 
         #Display the rect
-        pygame.draw.rect(self.position, self.reccolor, self.rect, border_radius=10)
-
+        pygame.display.flip()
