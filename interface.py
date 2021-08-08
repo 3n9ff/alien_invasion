de52cf54b357eb5e264f820ea7dda5211fef30aa
3n9ff <979pol@gmail.com>
2021-08-08 11:33:48 +0200

    seguir haciendo indicdor de vidas(casi acabado)

---
+++ interface.py
@@ -0,0 +1,90 @@
+import pygame
+from pygame import draw
+
+class Button:
+    """Create all the butons"""
+    
+    def __init__(self, ai_game, msg, letter_size = 50):
+
+        #windows info
+        self.screen = ai_game.screen
+        self.screen_rect = self.screen.get_rect()
+
+        #Text info
+        self.txcolor = (0, 255, 0)
+        self.font = pygame.font.SysFont(None, letter_size)
+        self.txrender = self.font.render( msg, True, self.txcolor, (0,0,0))
+
+        #text rect info
+        self.txrect = self.txrender.get_rect()
+        self.txrect.centerx = self.screen_rect.centerx
+        self.txrect.centery = self.screen_rect.centery
+
+        #Rect info
+        self.reccolor = (0,0,0) 
+        self.rectangulo = pygame.draw.rect(self.screen, self.reccolor, self.txrect, border_radius=10)       
+
+    def draw_button(self):
+        """Draw the start button"""
+
+        #Display the text
+        self.screen.blit(self.txrender, self.txrect)
+
+class ScoreBoard:
+    """Create a scoreboard by inputing the score"""
+
+    def __init__(self, ai_game, stats ):
+
+        #Screen info
+        self.screen = ai_game.screen
+        self.screen_rect = self.screen.get_rect()
+
+        #Text info
+        self.score = str(stats)
+        self.font = pygame.font.SysFont(None, 50)
+        self.text = self.font.render(self.score, True, (0, 0, 0))
+        self.txrect = self.text.get_rect()
+
+        #txrect data
+        self.rect = pygame.Rect(self.txrect)
+        pygame.draw.rect(self.screen, (25, 25, 112), self.rect)
+
+    def _show_scoreboard(self,):
+        """Draw the scoreboard onto the screen, 
+        change the self.txrect value to set the position"""
+
+        #Set the position
+        self.x, self.y = self.txrect.x, self.txrect.y + 20
+
+        #draw
+        self.screen.blit(self.text, (self.x, self.y))
+
+class LivesIndicator:
+    """Drae on the screen how many lives are left"""
+
+    def __init__(self,ai_game):
+        
+        self.ai_game = ai_game
+
+        #screen info
+        self.screen = ai_game.screen
+        self.screen_rect = self.screen.get_rect()
+
+        #image 
+        self.mum = pygame.image.load('imagenes/nave.png')
+        image = self.mum.convert()
+        self.image = pygame.transform.scale(image, (40, 34))
+        self.image_rect = self.image.get_rect()
+
+        #image position
+        self.image_width = self.image_rect.width
+        self.position_x = 0 + self.image_width * self.ai_game.statistic.ship_lives_left + 5
+
+    def draw_lives(self):
+        """Blit the lives"""
+
+        for live in self.ai_game.statistic.ship_lives_left:
+
+            self.screen.blit(self.image, (self.position_x, 20))
+
+            #Continuar x akii, en teoria ya esta
